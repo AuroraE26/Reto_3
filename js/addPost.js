c2a658fc040a3fe2a83fe0fdb4b536e325678156
Aurora <unefleur26@gmail.com>
2021-11-03 17:32:15 -0600

    addPost con title, cover, content e img

--- js/addPost.js
+++ js/addPost.js
@@ -1,35 +1,53 @@
-
-function addPost(){
-    let inputTitle=document.querySelector("input[name='title']");
-    let inputTags=document.querySelector("input[name='tags']");
-    let inputContent=document.querySelector("input[name='content']");
-    let inputImg=document.querySelector("input[name='image']");
-    let inputDate=document.querySelector("input[name='trip-start']");
-    
-    let title=inputTitle.value;
-    let tags=inputTags.value;
-    let content=inputContent.value;
-    let image=inputImg.value;
-    let date=inputDate.value;
-
-    objData={
-        title:"",
-        tags:["",""],
-        content:"",
-        image:"",
-        date:"",
-        reactions:{
-            likes:0,
-            unicorn:0,
-            save:0,
-        },
-    }
-
-    const xhr=new XMLHttpRequest();
-    const URL=`https://reto-3-77f5a-default-rtdb.firebaseio.com/.json`;
-    xhr.open("POST",URL,true);
-    xhr.send(JSON.stringify({title,tags,content,image, date}))
+objData={
+    title:"",
+    cover:"",
+    //tags:["",""],
+    content:"",
+    image:"",
+    //date:"",
+    /*reactions:{
+        likes:0,
+        unicorn:0,
+        save:0,
+    },
+    */
 }
 
-const btnAdd=document.querySelector("#btnAddPost");
-btnAdd.addEventListener("click", addPost);
+
+document.getElementById('btnCover').onclick = function addCover(){
+    let promptCover=prompt("Add a cover image"); 
+    document.getElementById("pCover").value= promptCover;
+}
+
+document.getElementById('btnImg').onclick = function addImg(){
+    let promptIMG=prompt("Add an image"); 
+    document.getElementById("pIMG").value= promptIMG;
+}
+
+document.getElementById('btn3').onclick = function fillObj(objData){
+    
+    
+    
+    let inputTitle=document.querySelector("input[name='title']");
+    //let inputTags=document.querySelector("input[name='tags']");
+    let inputContent=document.querySelector("input[name='content']");
+    let inputCover=document.querySelector("p[name='PCOVER']");
+    let inputImg=document.querySelector("p[name='pIMG']");
+    //let inputDate=document.querySelector("input[name='trip-start']");
+    
+    objData.title=inputTitle.value;
+    objData.content=inputContent.value; 
+    objData.cover=inputCover.value;
+    objData.image=inputImg.value;
+
+    const xhr=new XMLHttpRequest();
+    const URL=`https://reto-3-77f5a-default-rtdb.firebaseio.com/-MnbgWug_8R5bbGqe60X.json`;
+    xhr.open("POST",URL,true);
+    xhr.send(JSON.stringify({title:(objData.title), content:(objData.content), cover:(objData.cover),
+    image:(objData.image)}));
+    console.log("SEND");
+    console.log(objData)
+    
+}
+
+
